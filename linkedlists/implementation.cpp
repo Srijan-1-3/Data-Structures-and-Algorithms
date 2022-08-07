2f2790e0979802ad897520eec840938b39136f99
Siddhant Dudhane <siddhantdudhane21@gmail.com>
2022-08-07 19:31:44 +0530

    Linked ist implemented with all operations

--- linkedlists/implementation.cpp
+++ linkedlists/implementation.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 
+void prepend(int data);
+
 class Node{
     public:
 	int data;
@@ -11,7 +13,56 @@ class Node{
 
 Node *head=NULL;
 
-void insert(int data){
+void insert(int data,int pos){
+
+ Node *new_node = new Node();
+ new_node->data = data;
+
+ Node *ptr=head;
+
+if(pos<0){
+	cout<<"Invalid input";
+}
+else if(pos==0){
+	prepend(data);
+}
+else{
+for(int i=0;i<pos-2;i++)
+ptr = ptr->next;
+
+new_node->next = ptr->next;
+
+ptr->next = new_node;
+
+ }
+ 
+}
+
+void remove(int pos)
+{
+
+Node * ptr=head;
+int i;
+
+for(i=0;i<pos-2;i++)
+ptr = ptr->next;
+
+ptr->next = ptr->next->next;
+
+
+}
+
+void prepend(int data){
+
+	Node *new_node = new Node();
+	new_node->data = data;
+	
+	new_node->next = head;
+
+	head = new_node;
+}
+
+void append(int data){
 
    Node *ptr;
    Node *new_node = new Node;
@@ -58,12 +109,24 @@ int main(){
    cout<<"Enter the elements : "<<endl;
    for(int i=0;i<n;i++){
    	cin>>data;
-   	insert(data);
+   	append(data);
    }
    
-
+   prepend(9);
+   prepend(1);
     
    cout<<"The linked list is : ";
+   display();
+   cout<<endl;
+
+   insert(3,3);
+   cout<<"The linked list after insertion is : "<<endl;
+   display();
+   cout<<endl;
+
+
+   remove(3);
+   cout<<"The linked list after deletion is : "<<endl;
    display();
    cout<<endl;
    
